0d89add63277d35d1dc2cadbfa6a7cda7781e0ff
kavdihodgson <kavdyjh@gmail.com>
2018-01-22 15:36:14 -0800

    started last digit kata.

---
+++ katas/last_digit.py
@@ -0,0 +1,8 @@
+"""Module for function that takes in two numbers a and b and returns the last decimal digit of a^b."""
+
+
+def last_digit(n1, n2):
+    """Return last digit of n1^n2."""
+    x = n1 ** n2
+    answer = int(repr(x)[-1])
+    return answer
