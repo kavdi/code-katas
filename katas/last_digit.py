e7ebd7d6dd3126eddee3e7950e8cc30ac6ae06b7
kavdihodgson <kavdyjh@gmail.com>
2018-01-22 16:00:02 -0800

    last number module not working with long numbers.

--- katas/last_digit.py
+++ katas/last_digit.py
@@ -6,3 +6,6 @@ def last_digit(n1, n2):
     x = n1 ** n2
     answer = int(repr(x)[-1])
     return answer
+ 
+
+ """This is not working for large numbers. Need to refactor code to work properly."""
