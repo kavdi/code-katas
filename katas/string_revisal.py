19de60c1a5a88604648026a838d976b16d96d7ae
kavdihodgson <kavdyjh@gmail.com>
2018-01-23 14:00:22 -0800

    got string revisal working... much simpler than i thought.

--- katas/string_revisal.py
+++ katas/string_revisal.py
@@ -6,33 +6,11 @@ def dup(arry):
     answer = []
     for i in arry:
         x = list(i)
-        z = 0
-        import pdb; pdb.set_trace()
-        while z < len(x) - 1:
-            if x[z] == x[z + 1]:
-                x.remove(x[z + 1])
-            else:
-                print(len(x))
-                print(x)
-                z += 1
-        x = ''.join(x)
-        answer.append(x)
-    return answer
-
-
-def dup2(arry):
-    answer = []
-    cur = []
-    for i in arry:
-        x = list(i)
-        z = 0
-        import pdb; pdb.set_trace()
-        while z < len(x) - 1:
-            if x[z] != x[z + 1]:
-                cur.append(x[z])
-            elif x[z] == x[z + 1]:
-                z += 1
+        cur = []
+        for i in range(len(x) - 1):
+            if x[i] != x[i + 1]:
+                cur.append(x[i])
+        cur.append(x[len(x) - 1])
         word = ''.join(cur)
         answer.append(word)
-        cur = []
     return answer
