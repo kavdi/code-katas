3c545c34b3525b22c39881026e54e120ecad1220
kavdihodgson <kavdyjh@gmail.com>
2018-03-06 15:31:22 -0800

    function working, need to write tests.

--- katas/longest_substring.py
+++ katas/longest_substring.py
@@ -4,12 +4,12 @@
 def lengthOfLongestSubstring(string):
     """Give back length of longest substring."""
     used = {}
-    max_length = start = 0
+    longest = start = 0
     for i, c in enumerate(string):
         if c in used and start <= used[c]:
             start = used[c] + 1
         else:
-            max_length = max(max_length, i - start + 1)
+            longest = max(longest, i - start + 1)
 
         used[c] = i
-    return max_length
+    return longest
